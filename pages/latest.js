01827fbb667b3faffc3b32293e124a57f6cc9f99
Jan Kaßel <jan@kassel.works>
2020-02-27 00:23:18 +0100

    equip /latest route with 301 redirect

--- pages/latest.js
+++ pages/latest.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import builds from "../builds.json";
 
 const Latest = () => {
   const router = useRouter();
-  useEffect(() => {
-    router.push(`/${builds.shift().filename}`);
-  }, []);
+  router.push(`/${builds.shift().filename}`);
 
   return null;
+};
+
+Latest.getInitialProps = ({ res }) => {
+  if (typeof window === "undefined") {
+    res.writeHead(301, {
+      Location: `/${builds.shift().filename}`
+    });
+    res.end();
+    res.finished = true;
+  }
+
+  return {};
 };
 
 export default Latest;
